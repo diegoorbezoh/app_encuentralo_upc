f49350f83bdff2effa6f92b5bb295880b13a9d64
diegoorbezoh <31303100+diegoorbezoh@users.noreply.github.com>
2020-11-29 11:12:34 -0600

    Mackup Menú 1 terminado

---
+++ app/src/main/java/com/example/app_encuentralo/comparador_precios_resultado.java
@@ -0,0 +1,64 @@
+package com.example.app_encuentralo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link comparador_precios_resultado#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class comparador_precios_resultado extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public comparador_precios_resultado() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment comparador_precios_resultado.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static comparador_precios_resultado newInstance(String param1, String param2) {
+        comparador_precios_resultado fragment = new comparador_precios_resultado();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_comparador_precios_resultado, container, false);
+    }
+}
