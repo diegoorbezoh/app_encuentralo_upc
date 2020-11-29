80e09ec934d9d74da9dc99a4e34c5035e4d14e80
diegoorbezoh <31303100+diegoorbezoh@users.noreply.github.com>
2020-11-29 00:41:19 -0600

    Desarrollo Inicio

--- app/src/main/java/com/example/app_encuentralo/inicio.java
+++ app/src/main/java/com/example/app_encuentralo/inicio.java
@@ -1,4 +1,30 @@
 package com.example.app_encuentralo;
 
-public class inicio {
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class inicio extends Fragment {
+
+    public inicio() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inicio, container, false);
+    }
 }
